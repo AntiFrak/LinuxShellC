2fa4aae800ca081eb77079a2e3a2f5343e5ec96b
oneyan1 <yangrodno@gmail.com>
2019-04-14 18:43:34 +0200

    add function execute, but have a bug 'not such file in directory' i dont know wtf

--- src/main.c
+++ src/main.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "LibFunction/readLineShell.h"
 #include "LibFunction/parsingLineShell.h"
 
 #define EXIT_CODE 0
 #define BUF_SIZE 128
-#define TOKEN_SEP " \t\n\r"
+
+void executeCommand(int number,char *tokenTab[]);
 
 void shellCycle(){
     char *line = readLine();
     char *tokenTab[BUF_SIZE];
-    int arg;
+    int number, stat;
 
     while(line != NULL){
-        arg = parsingLine(line, tokenTab);
+        number = parsingLine(line, tokenTab);
+        executeCommand(number, tokenTab);
         free(line);
         line = readLine();
     }
 }
 
-
 int main(){
     shellCycle();
     return EXIT_CODE;
 }
+
+void executeCommand(int number, char *tokenTab[]){
+    pid_t pid, wpid;
+    int  wstatus;
+    //char *argv[2]; // для поиска ебучего бага
+    //argv[1]=NULL;// для поиска ебучего бага
+
+   
+    //for(int i = 0; i< number; i++){
+       // argv[0]= tokenTab[i];
+       // printf("%s , %d", tokenTab[i], number);
+        pid = fork();
+        if(pid == 0){
+            if(execvp(tokenTab[0], tokenTab)== -1){  // тут поидее должно быть tokenTab вторым параметром, для теста юзал argv
+                perror("execvp");
+                exit(-1);
+            }
+        }
+        if(pid < 0){
+            perror("fork2");
+        }
+        else{
+            do{
+                wpid = waitpid(pid, &wstatus, WUNTRACED);
+            }while(!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+        }
+   // }
+    
+}
+
